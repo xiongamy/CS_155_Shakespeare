591eb3b0cfdc06910ee71cc3bac1c72c28e3c50d
Hippopotas <magichatman2@gmail.com>
2018-03-09 20:27:38 -0800

    stuff

--- models/republican_net_neurality.py
+++ models/republican_net_neurality.py
@@ -44,16 +44,17 @@ for s in sonnets:
     all_char += s
 chars = np.insert(np.unique(all_char), 1, '')
 cdict = {c: i for i, c in enumerate(chars)}
+cdict_rev = {i: c for i, c in enumerate(chars)}
 
 x_train = []
 y_train = []
 for sonnet in sonnets:
     for i in range(40):
         blanks = [''] * (40 - i)
-        x_train.append(blanks + sonnet[:i])
+        x_train.append([blanks + sonnet[:i]])
         y_train.append(sonnet[i])
     for i in range(len(sonnet) - 40):
-        x_train.append(sonnet[i:i + 40])
+        x_train.append([sonnet[i:i + 40]])
         y_train.append(sonnet[i + 40])
 
 # One-hot encoding the labels
@@ -61,12 +62,17 @@ for i, y in enumerate(y_train):
     y_train[i] = cdict[y]
 y_train = keras.utils.np_utils.to_categorical(y_train)
 
+for x in x_train:
+    for x2 in x:
+        for i, c in enumerate(x2):
+            x2[i] = cdict[c]
+
 np_x = np.array(x_train)
 
 ### RNN (Single LSTM layer with ~150 cells)
 
 model = Sequential()
-model.add(LSTM(150, input_shape=(40,)))
+model.add(LSTM(150, input_shape=(1, 40)))
 
 model.add(Dense(len(chars)))
 model.add(Activation('softmax'))
@@ -77,7 +83,7 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # Train the model, iterating on the data in batches of 32 samples
-history = model.fit(np_x, y_train, epochs=10, batch_size=32, verbose=0)
+history = model.fit(np_x, y_train, epochs=5000, batch_size=32, verbose=1)
 
 
 ### Predictions
@@ -86,8 +92,16 @@ new_sonnet = np.full(40, '')
 next_char = ''
 while next_char != 'END':
     sl = len(new_sonnet)
-    next_char = model.predict_classes(x=new_sonnet[s1 - 40:s1])
-    new_sonnet.append(next_char)
+    sonnet_vals = np.zeros((1, 1, 40))
+    for i, c in enumerate(new_sonnet[sl - 40:sl]):
+        sonnet_vals[0, 0, i] = cdict[c]
+    np_test = np.array(sonnet_vals)
+
+    next_val = model.predict_classes(x=np_test)[0]
+
+    next_char = cdict_rev[next_val]
+    print(next_char, end=' ')
+    new_sonnet = np.append(new_sonnet, next_char)
 
 print(new_sonnet)
 '''
