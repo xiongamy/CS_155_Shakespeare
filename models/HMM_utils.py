42f90ccae893fecf090947a42ce6e93462b8f4f2
xiongamy <xiong.yamy@gmail.com>
2018-03-09 19:10:39 -0800

    Add rhyme dictionary

--- models/HMM_utils.py
+++ models/HMM_utils.py
@@ -1,8 +1,10 @@
+punctuation = [',', '\'', ':', '.', '?', ';', '(', ')', '!']
+
 class SyllableDict:
 
     def __init__(self, fname='../data/Syllable_dictionary.txt'):
         self.dict = {',' : {0}, '\'' : {0}, ':' : {0}, '.' : {0}, '?' : {0}, ';' : {0}, '(' : {0}, ')' : {0}, '!' : {0}}
-        self.words = [',', '\'', ':', '.', '?', ';', '(', ')', '!']
+        self.words = punctuation[:]
         self.index_of = {',' : 0, '\'' : 1, ':' : 2, '.' : 3, '?' : 4, ';' : 5, '(' : 6, ')' : 7, '!' : 8}
         current_index = 9
         
@@ -61,8 +63,93 @@ class SyllableDict:
     
     def contains_word(self, word):
         return word in self.dict
+
+
+
+class RhymeDict:
+    def __init__(self, training_data):
+    
+        def get_first_word(line):
+            # skip punctuation to get the first word
+            for j in range(len(line)):
+                if not line[j] in punctuation:
+                    return line[j]
+            return ''
+            
+        # rhyme scheme is abab cdcd efef gg
+        rhyme_lines = [0, 1, 4, 5, 8, 9, 12]
+        
+        rhyme_sets = []
+        word_to_sets = {}
+        for i in range(len(training_data)):
+            if not i % 14 in rhyme_lines:
+                continue
+                
+            word = get_first_word(training_data[i])
+            if i % 14 == 12:
+                rhyme_word = get_first_word(training_data[i + 1])
+            else:
+                rhyme_word = get_first_word(training_data[i + 2])
+                
+            if word == '' or rhyme_word == '':
+                continue
+                
+            # update the rhyme sets
+            word_in_set = False
+            rhyme_in_set = False
+            if word in word_to_sets:
+                word_in_set = True
+            if rhyme_word in word_to_sets:
+                rhyme_in_set = True
+            
+            if word_in_set and rhyme_in_set:
+                # are they in different sets?
+                set_index = word_to_sets[word]
+                if word_to_sets[rhyme_word] != set_index:
+                    # combine sets together
+                    other_index = word_to_sets[rhyme_word]
+                    other_set = rhyme_sets[other_index]
+                    rhyme_sets[set_index] |= other_set
+                    
+                    # update word_to_sets for all words in the other set
+                    for key in other_set:
+                        word_to_sets[key] = set_index
+                    
+                    # remove the other set and update set indices
+                    rhyme_sets.remove(other_set)
+                    for key, value in word_to_sets.items():
+                        if value > other_index:
+                            word_to_sets[key] = value - 1
+            elif word_in_set:
+                # add rhyme_word to word's set
+                set_index = word_to_sets[word]
+                rhyme_sets[set_index].add(rhyme_word)
+                word_to_sets[rhyme_word] = set_index
+            elif rhyme_in_set:
+                # add word to rhyme_word's set
+                set_index = word_to_sets[rhyme_word]
+                rhyme_sets[set_index].add(word)
+                word_to_sets[word] = set_index
+            else:
+                # create new set
+                new_set = set([word, rhyme_word])
+                set_index = len(rhyme_sets)
+                rhyme_sets.append(new_set)
+                word_to_sets[word] = set_index
+                word_to_sets[rhyme_word] = set_index
+        
+        self.rhyme_sets = rhyme_sets
+        self.word_to_sets = word_to_sets
         
 
+    def get_all_rhyming_words(self):
+        all_words = set([])
+        for set in self.rhyme_sets:
+            all_words |= set
+        return all_words
+    
+    def get_rhymes(self, word):
+        return self.rhyme_sets[self.word_to_sets[word]]
         
 
 def get_training_data(syllable_dict):
@@ -88,16 +175,28 @@ def get_training_data(syllable_dict):
         
     # read lines from the Shakespearan sonnet file
     lines = []
+    skip_sonnet = False
     with open('../data/shakespeare.txt', 'r') as f:
         for line in f:
             line = line.strip()
+            
+            if skip_sonnet:
+                # skip lines until you reach another digit
+                if line.isdigit():
+                    skip_sonnet = False
+                continue
+            
+            # skip sonnets 99 and 126
+            if line == '99' or line == '126':
+                skip_sonnet = True
+                continue
+            
             # ignore blank lines and the sonnet's number
             if not line.isdigit() and line != '':
                 lines.append(line)
             
     # convert each line to a list of words
     data_strings = []
-    punctuation = [',', '\'', ':', '.', '?', ';', '(', ')', '!']
     for line in lines:
         words = line.split()
         words_and_punct = []
@@ -106,7 +205,8 @@ def get_training_data(syllable_dict):
         for word in words:
             word = word.lower()
             find_word_parts(word, words_and_punct)
-                
+        
+        words_and_punct.reverse()
         data_strings.append(words_and_punct)
         
     # convert the words to their IDs
